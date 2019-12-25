f6ba95e13bceb42161a76d968966fea8caa299a5
fan.yang <34465021+jwfl724168@users.noreply.github.com>
2019-12-25 15:38:38 +0800

    学习 数组遍历

--- src/main/kotlin/com/goat/kotlin/chapter151/A01.kt
+++ src/main/kotlin/com/goat/kotlin/chapter151/A01.kt
@@ -9,8 +9,8 @@ class A01{
     @Test
     fun test1() {
         var arrayOf = arrayOf(1, 2, 3)
-        arrayOf.forEach{x-> println(x)}
-        arrayOf.forEach{ print(it) }
+        arrayOf.forEach{ x-> println(x) }
+        arrayOf.forEach{ println(it) }
     }
 
     // 数组初始化赋值
@@ -26,12 +26,24 @@ class A01{
     @Test
     fun test3() {
         // 定义int型数组
-        val intArrayOf = intArrayOf(1, 2, 3)
+        val array = intArrayOf(1, 2, 3)
         //intArrayOf = intArrayOf(2,3) // 赋值报错
         // 将数组的第二个元素改成4
-        intArrayOf.set(1,4)
-        // 遍历数组
-        intArrayOf.forEach { x-> print(x) }
+        array.set(1,4)
+        // 遍历数组 1
+        array.forEach { x-> println(x) }
+        // 遍历数组 2
+        for (item in array){
+            println(item)
+        }
+        // 遍历数组 3
+        for (i in array.indices){
+            println("array[$i] = ${array[i]}")
+        }
+        // 遍历数组 4
+        for ((index,value) in array.withIndex()){
+            println("array[$index] = $value")
+        }
     }
 
 }
